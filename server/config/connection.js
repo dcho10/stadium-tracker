cb31e237b047199e99cbd684a30f277b337ec193
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-10-20 15:11:24 -0400

    Removed URL parser

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,11 +4,9 @@ const mongoose = require("mongoose");
 // Connect to the MongoDB URI from environment variable or fallback to localhost
 const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/visitor_DB';
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log(`Connected to MongoDB at ${mongoURI}`))
-.catch((err) => console.error('MongoDB connection error:', err));
+mongoose.connect(mongoURI)
+  .then(() => console.log(`Connected to MongoDB at ${mongoURI}`))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
+
